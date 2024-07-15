a5cd9eb0db0d0657bdda0faec771ffb33cbe481d
yuroulan <dokgomincho1810@gmail.com>
2024-07-15 17:52:01 +0800

    allpdscodes

--- Python/iDesign_iAssess/Topic14_ExtractDatafromWebsite/iD_T14_Q2.py
+++ Python/iDesign_iAssess/Topic14_ExtractDatafromWebsite/iD_T14_Q2.py
@@ -20,3 +20,42 @@ Scraped Data:
   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
 '''
+import requests
+import itertools 
+
+userAgents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1"
+]
+
+userAgentCycle = itertools.cycle(userAgents)
+
+def make_request(url):
+    userAgent = next(userAgentCycle)
+
+    headers = {
+        'User-Agent' : userAgent
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            return response.content.decode('utf-8')
+        else:
+            return f"Failed to retrieve content: Status code {response.status_code}"
+    except Exception as e:
+        return f"An error occured: {e}"
+
+def main():
+    url = input("Enter the URL to scrape:")
+    scrapedData = make_request(url)
+
+    print("Scraped Data: \n")
+    print(scrapedData)
+
+if __name__ == "__main__":
+    main()
---
+++ Python/iDesign_iAssess/Topic14_ExtractDatafromWebsite/iD_T14_Q7.py
@@ -0,0 +1,22 @@
+# Problem 7
+'''
+Books Details Extraction
+
+Write a python program to get the item name, link and price of the book and display it.
+
+Method	Description
+def find_item_name()        	This method reads the item name and displays the item name.
+def find_item_link()	This method displays the item link.
+def find_item_price	This methos displays the item price.
+Input format:
+ HTML code is given in a template code.
+Output format:
+Prints the item name, link and price. 
+
+Sample Input and Output 1:
+
+A Light in the Attic
+catalogue/a-light-in-the-attic_1000/index.html
+£51.77
+'''
+
